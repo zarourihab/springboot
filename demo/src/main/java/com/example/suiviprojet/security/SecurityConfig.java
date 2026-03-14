2f8948cbf43897fc50b7de1e2a203e20cecb9912
Saadia El Mouden <saadiae474@gmail.com>
2026-03-14 19:23:37 +0000

    Ajout des screenshots des tests Postman

--- demo/src/main/java/com/example/suiviprojet/security/SecurityConfig.java
+++ demo/src/main/java/com/example/suiviprojet/security/SecurityConfig.java
@@ -24,13 +24,22 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
-
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/employes").permitAll()
+                        .requestMatchers("/api/employes/**").permitAll()
+                        .requestMatchers("/api/organismes/**").permitAll()
+                        .requestMatchers("/api/projets/**").permitAll()
+                        .requestMatchers("/api/phases/**").permitAll()
+                        .requestMatchers("/api/affectations/**").permitAll()
+                        .requestMatchers("/api/livrables/**").permitAll()
+                        .requestMatchers("/api/documents/**").permitAll()
+                        .requestMatchers("/api/factures/**").permitAll()
+                        .requestMatchers("/api/reporting/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
-
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
